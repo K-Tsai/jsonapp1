f724b16565d93d6706527ef2f3054f57a95ecf45
Kevin Tsai <k.tsai92@gmail.com>
2020-09-21 16:00:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from 'react-jsonschema-form';
+import "./App.css" 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mySchema ={
+  "title": "Dog License Application",
+  "description": "Dogs must be registered in the city before April 1st each year Please fill out this form",
+  "type": "object",
+  "required": [
+    "firstName",
+    "lastName",
+    "address",
+    "email",
+    "homePhone"
+  ],
+  "properties": {
+    "firstName": {
+      "type": "string",
+      "title": "Owner First Name",
+    },
+    "middleInitial": {
+      "type": "string",
+      "title": "Middle Initial"
+    },
+    "lastName": {
+      "type": "string",
+      "title": "Owner Last Name"
+    }
+}
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit= this.handleSubmit.bind(this);
+  }
+
+  handleSubmit({formData}) {
+    console.log(formData)
+  }
+
+  render() {
+    return (
+      <div>
+        <Form schema={mySchema} onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
